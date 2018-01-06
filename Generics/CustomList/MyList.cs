d5ec22c842c4cc412a8b9417e247db090b1f6171
PlamenYordanov <yordanov.plamen@abv.bg>
2018-01-06 20:49:32 +0200

    custom list crappy solution 100/100

---
+++ Generics/CustomList/MyList.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomList
+{
+    public class MyList<T> : IEnumerable where T : IComparable
+    {
+
+        private List<T> list = new List<T>();
+
+        public List<T> List { get { return this.list; } }
+
+        public void Add(T element)
+        {
+            list.Add(element);
+        }
+        public T Remove(int index)
+        {
+            T element = list[index];
+            list.RemoveAt(index);
+            return element;
+        }
+        public bool Contains(T element)
+        {
+            return list.Contains(element);
+        }
+        public void Swap(int index1, int index2)
+        {
+            T firstElement = list[index1];
+            list[index1] = list[index2];
+            list[index2] = firstElement;
+        }
+        public int CountGreaterThan(T element)
+        {
+            var count = list.Where(x => x.CompareTo(element) > 0).Count();
+            return count;
+        }
+        public T Max()
+        {
+            return list.Max();
+        }
+        public T Min()
+        {
+            return list.Min();
+        }
+        public void Sort()
+        {
+            this.list.Sort();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return list.GetEnumerator();
+        }
+
+        
+    }
+}
---
+++ Generics/CustomList/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace CustomList
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var list = new MyList<string>();
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                var tokens = input.Split();
+                var command = tokens[0];
+                switch (command)
+                {
+                    case "Add":
+                        var element = tokens[1];
+                        list.Add(element);
+                        break;
+                    case "Remove":
+                        var index = int.Parse(tokens[1]);
+                        list.Remove(index);
+                        break;
+                    case "Contains":
+                        element = tokens[1];
+                        Console.WriteLine(list.Contains(element));
+                        break;
+                    case "Greater":
+                        element = tokens[1];
+                        Console.WriteLine(list.CountGreaterThan(element));
+                        break;
+                    case "Swap":
+                        var index1 = int.Parse(tokens[1]);
+                        var index2 = int.Parse(tokens[2]);
+                        list.Swap(index1, index2);
+                        break;
+                    case "Max":
+                        Console.WriteLine(list.Max());
+                        break;
+                    case "Min":
+                        Console.WriteLine(list.Min());
+                        break;
+                    case "Print":
+                        foreach (var item in list)
+                        {
+                            Console.WriteLine(item);
+                        }
+                        break;
+                    case "Sort":
+                        list.Sort();
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+        }
+        
+    }
+}
