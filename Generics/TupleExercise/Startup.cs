689e829afe82e372947f4eae074bf3aafdf580ca
PlamenYordanov <yordanov.plamen@abv.bg>
2018-01-07 13:19:57 +0200

    Tuple 100/100

---
+++ Generics/TupleExercise/Startup.cs
@@ -0,0 +1,28 @@
+﻿namespace TupleExercise
+{
+    using System;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            var nameAddress = Console.ReadLine().Split();
+            var fullName = $"{nameAddress[0]} {nameAddress[1]}";
+            var address = nameAddress[2];
+            var nameTuple = new Tuple<string, string>(fullName, address);
+            Console.WriteLine(nameTuple);
+
+            var nameBeer = Console.ReadLine().Split();
+            var name = nameBeer[0];
+            var beerAmount = int.Parse(nameBeer[1]);
+            var beerTuple = new Tuple<string, int>(name, beerAmount);
+            Console.WriteLine(beerTuple);
+
+            var intDouble = Console.ReadLine().Split();
+            var intValue = int.Parse(intDouble[0]);
+            var doubleValue = double.Parse(intDouble[1]);
+            var intDoubleTuple = new Tuple<int, double>(intValue, doubleValue);
+            Console.WriteLine(intDoubleTuple);
+        }
+    }
+}
