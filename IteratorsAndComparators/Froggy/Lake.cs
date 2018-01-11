5461319c91b7b35f2ceb7a24711130922a0d80d6
PlamenYordanov <yordanov.plamen@abv.bg>
2018-01-11 20:01:15 +0200

    Froggy 100/100

---
+++ IteratorsAndComparators/Froggy/Lake.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private List<int> Stones { get; set; }
+
+        public Lake(List<int> stones)
+        {
+            this.Stones = new List<int>();
+            this.Stones = stones;
+
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            int length = this.Stones.Count;
+            for (int i = 0; i < length; i+=2)
+            {
+                yield return this.Stones[i];
+            }
+            var reverseCount = length % 2 == 0 ? length : length - 1;
+            for (int i = reverseCount - 1; i >= 0; i -= 2)
+            {
+                yield return this.Stones[i];
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", this.Stones);
+        }
+    }
+}
---
+++ IteratorsAndComparators/Froggy/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Froggy
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var stones = Console.ReadLine().Split(new[] { ' ', ',' }
+            , StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse).ToList();
+
+            var lake = new Lake(stones);
+
+            lake = Cross(lake);
+
+            Console.WriteLine(lake);
+        }
+        private static Lake Cross(Lake lake)
+        {
+            var crossedLake = new List<int>();
+            foreach (var stone in lake)
+            {
+                crossedLake.Add(stone);
+            }
+            return new Lake(crossedLake);
+        }
+    }
+}
