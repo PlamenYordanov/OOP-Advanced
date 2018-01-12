3eea1c975de76491f1b69f8f2024b72cccc01064
PlamenYordanov <yordanov.plamen@abv.bg>
2018-01-12 13:27:18 +0200

    CompareObjects 100/100

---
+++ IteratorsAndComparators/ComparingObjects/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age, string town)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Town = town;
+        }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Town { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            if (this.Name != other.Name)
+            {
+                return this.Name.CompareTo(other.Name);
+            }
+            if (this.Age != other.Age)
+            {
+                return this.Age.CompareTo(other.Age);
+            }
+            return this.Town.CompareTo(other.Town);
+
+        }
+
+        public bool Equals(Person other)
+        {
+            return this.Name == other.Name &&
+                this.Age == other.Age &&
+                    this.Town == other.Town;
+        }
+
+        
+
+        public override string ToString()
+        {
+            return $"{this.Name} - {this.Age} - {this.Town}";
+        }
+    }
+}
---
+++ IteratorsAndComparators/ComparingObjects/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ComparingObjects
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            string input = string.Empty;
+            var people = new List<Person>();
+            while ((input = Console.ReadLine()) != "END")
+            {
+                var personData = input.Split();
+                var name = personData[0];
+                var age = int.Parse(personData[1]);
+                var town = personData[2];
+                var person = new Person(name, age, town);
+                people.Add(person);
+            }
+            var personIndex = int.Parse(Console.ReadLine());
+            var personToCompare = people[personIndex - 1];
+
+            int equalPeople = people.Where(x => x.Equals(personToCompare)).Count();
+            int notEqualPeople = people.Where(x => !x.Equals(personToCompare)).Count();
+            string result = equalPeople == 1 ? "No matches" : FormatPeople(equalPeople, notEqualPeople);
+            Console.WriteLine(result);
+        }
+
+        private static string FormatPeople(int equalPeople, int notEqualPeople)
+        {
+            int equalPeopleCount = equalPeople;
+            int notEqualPeopleCount = notEqualPeople;
+            int totalPeople = equalPeopleCount + notEqualPeopleCount;
+            return $"{equalPeopleCount} {notEqualPeopleCount} {totalPeople}";
+        }
+    }
+}
